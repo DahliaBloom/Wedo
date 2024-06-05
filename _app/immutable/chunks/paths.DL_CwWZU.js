bc80760cd32fd7216024c9a0f3559cc12dc335b8
Felix Steinbrecht <steinbrecht@proton.me>
2024-06-05 16:13:07 +0200

    Updates

--- _app/immutable/chunks/paths.DL_CwWZU.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1pw8suq)==null?void 0:s.base)??"/wedo";var e;const t=((e=globalThis.__sveltekit_1pw8suq)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/entry/start.nkqiFi4R.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.B9b7JWi2.js";export{t as start};
